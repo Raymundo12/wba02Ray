6790127518271e7eee89205051dcb96a88913c87
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-17 23:01:44 +0000

    saverts

--- models/user.js
+++ models/user.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose');
 var userSchema = new mongoose.Schema({
     email: { type: String, unique: true, lowercase: true},
     password: String,
-    username: string,
+    username: String,
     gender: {
          type: String,
          enum: ['MALE', 'FEMALE']
