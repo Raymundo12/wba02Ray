678dc4d130f9c9df1481bba20439656e21f27623
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-17 17:10:20 +0000

    Mongooseconection

--- index.js
+++ index.js
@@ -3,20 +3,11 @@ axios = require('axios'),
 logger = require('morgan'),
 cors = require('cors'),
 express = require('express'),
-bodyParser = require('body-parser');
+bodyParser = require('body-parser'),
 mongoose = require('mongoose');
 
 var app = express();
 var port = 8000;
-
-app.use(bodyParser.json());
-app.get('Hello/:foo:bar', (req, res) => {
-res.json({message: "Hello BScBest!", data: [
-   req.params.foo,
-   req.params.bar
-
-]});
-});
 
 app.use(logger('tiny'));
 app.use(require('./routes'));
@@ -35,7 +26,17 @@ let users = []; // names of users will be stored here
   } catch(error){
     console.log(error)
   }
-})()
+})();
+
+mongoose.connect('mondb://localhost/test');
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb Error: ', err);
+    process.exit();
+});
+
+mongoose.connection.on('connected', () => {
+});
 
 app.listen(port, function(err) {
 console.log('Listening on port: ' + port) ; 
