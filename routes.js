55e3e9b4b4449648fb0e428c550ae9eec6deed2a
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-17 18:08:52 +0000

    users

--- routes.js
+++ routes.js
@@ -1,13 +1,17 @@
 var express = require('express'),
 router = express.Router();
 
-router.get('/Hello/:foo/:bar', (req, res) => {
-    res.json({message: 'Hello Cabron', data: [
-        req.params.foo,
-        req.params.bar
-    ]});
-   
-});
+var itemCtrl = require('./item-controller'),
+userCtrl = require('./user-controller');
+
+router.get('hello', itemCtrl.getWorld);
+
+router.get('hello/foo/:bar', itemCtrl.getWorldParams );
+
+router.post('hello', itemCtrl.postWorld);
+
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
 
 router.post('/hello', (req, res) => {
     res.json({result: 'Post was sent'});
