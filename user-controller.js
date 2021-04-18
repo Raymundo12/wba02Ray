4ed60a04a4acb36eae114b4607d75bc42898b899
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-18 09:55:22 +0000

    fix 3

--- user-controller.js
+++ user-controller.js
@@ -2,7 +2,7 @@ var User = require('./models/user')
 
 exports.createUser = function(req, res) {
 var newuser = new User (req.body);
-newuser.save(function(err, user){
+newuser.save(function(err, user) {
   if (err) {
        res.status (400).json(err);
   }
@@ -13,46 +13,37 @@ newuser.save(function(err, user){
 };
 
 exports.getUsers = function(req, res) {
-    User.find({}, function (err, users){
-
+    User.find({}, function (err, users) {
         if (err) {
             res.status(400).json(err);
         }
-
     res.json(user);
     });
 };
 
 exports.getUsers = function(req, res) {
-    User.findOne({_id: req.params.id}, function (err, users){
-
+    User.findOne({_id: req.params.id}, function (err, users) {
         if (err) {
             res.status(400).json(err);
         }
-
     res.json(user);
     });
 };
 
 exports.updateUsers = function(req, res) {
-    User.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true},
-          function (err, users){
-
+    User.findByIdAndUpdate({_id: req.params.id}, req.body, {new: true}, function (err, users) {
         if (err) {
             res.status(400).json(err);
         }
-
     res.json(user);
     });
 };
 
 exports.deleteUsers = function(req, res) {
-    User.findOneAndRemove({_id: req.params.id}, function (err, users){
-
+    User.findByIdAndRemove({_id: req.params.id}, function (err, users) {
         if (err) {
             res.status(400).json(err);
         }
-
     res.json(user);
     });
 };
