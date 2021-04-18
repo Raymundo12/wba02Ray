33f1ff402d07698c6078800acfe83364f650e740
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-18 12:04:47 +0000

    image

--- image-controller.js
+++ image-controller.js
@@ -1,3 +1,19 @@
-var UPLOAD_PATH = require('./routes').UPLOAD_PATH
+const UPLOAD_PATH = require('./routes').UPLOAD_PATH
 Image = require('./models/image');
+path = require('path'),
+fs = require('fs'),
+del = require('del');
 
+exports.uploadImage = function(req, res)  {
+
+    let newImage = new Image ();
+    newImage.filename = req.filename;
+    newImage.originalName = req.file.originalName;
+    newImage.desc = req.body.desc;
+    newImage.setPointerCapture(err => {
+         if (err){
+             return res.sendStatus(400);
+         }
+         res.status(201).send({newImage})
+    });
+};
