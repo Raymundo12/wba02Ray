cf43fffe1daa0540f43038c8af06b03f28ef0e92
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-18 11:27:04 +0000

    image

---
+++ models/image.js
@@ -0,0 +1,11 @@
+var mongoose = require('mongoose');
+
+var imageSchema = new mongoose.Schema({
+    filename: String,
+    originalName: String,
+    desc: String
+},
+{  timestamps: true});
+
+module.exports = mongoose.model('image', imageSchema);
+
