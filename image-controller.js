97d22120915f8e28c00bf4623afe295eff427553
Raymundo12 <raymundo.olverap@gmail.com>
2021-04-18 12:45:40 +0000

    image

--- image-controller.js
+++ image-controller.js
@@ -1,5 +1,5 @@
-const UPLOAD_PATH = require('./routes').UPLOAD_PATH
-Image = require('./models/image');
+const UPLOAD_PATH = require('./routes').UPLOAD_PATH,
+Image = require('./models/image'),
 path = require('path'),
 fs = require('fs'),
 del = require('del');
--- routes.js
+++ routes.js
@@ -20,5 +20,6 @@ var multer = require("multer");
 var upload = multer({dest: module.exports.UPLOAD_PATH});
 var imageCtrl = require('./image-controller');
 
+router.post('./images', upload.single('image'), imageCtrl.uploadImage);
 
 module.exports = router;
