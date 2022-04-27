693fc5935d9ac4f48994b6e57e354ea9c6c54b3e
Raymundo12 <raymundo.olverap@gmail.com>
2022-04-27 13:20:40 +0000

    mongoose

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const http = require("http");
      logger = require("morgan"),
      express = require ("express"), 
      bodyParser = require("body-parser");
+     mongoose = require('mongoose');
 
 
 let app = express();
@@ -13,4 +14,15 @@ app.use(logger("tiny"));
 
 app.listen(port, function(err){
     console.log("listening on port: "+ port)
+
+const dbURI = "mongodb://localhost/test";
+
+mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
+.then((result) => console.log('connected to db'))
+.catch((err) => console.log(err));
+
+
+
+
+
 });
