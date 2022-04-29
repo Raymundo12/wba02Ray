2269370a1177d23bd1a4b694d9fec8c7091e7259
Raymundo12 <raymundo.olverap@gmail.com>
2022-04-29 16:15:07 +0000

    Marina

--- models/user.js
+++ models/user.js
@@ -1,6 +1,15 @@
-
 const mongoose = require('mongoose'); 
+
+
+var imageSchema = new mongoose.Schema({ 
+    filename: String, 
+    originalName: String, 
+    desc: String, 
+    created: Date 
+}); 
  
+module.exports = mongoose.model('Image', imageSchema);
+
   const userSchema = new mongoose.Schema({  
       email: { type: String, unique: true, lowercase: true}, 
       password: String, 
--- routes.js
+++ routes.js
@@ -2,8 +2,20 @@ const express = require("express"),
       router = express.Router(),
       itemCtrl = require("./item-controller");
       userCtrl = require("./user-controller");
-
+      
+      module.exports.UPLOAD_PATH = 'uploads'; 
+ 
+      var multer = require('multer'), 
+      upload = multer({ dest: module.exports.UPLOAD_PATH }), 
+      imageCtrl = require('./image-controller');
+            
  
       router.get("/:foot/:bar", itemCtrl.helloWorld);
+
+router.post('/images', upload.single('image'), imageCtrl.uploadImage); 
+router.get('/images', imageCtrl.getImages); 
+router.get('/images/:id', imageCtrl.getImage); 
+router.delete('/images/:id', imageCtrl.deleteImage);
+
 
 module.exports = router;
