dee5ba26af1c5f2fb38432937b111216fa044046
Raymundo12 <raymundo.olverap@gmail.com>
2022-04-29 15:52:05 +0000

    installing multer

--- user-controller.js
+++ user-controller.js
@@ -12,3 +12,12 @@ exports.createUser = function(req, res) {
 }); 
 };
 
+exports.getUsers = function(req, res) { 
+    User.find({}, function (err, users) { 
+      if (err) { 
+        res.status(400).json(err);  
+      }  
+      res.json(users); 
+    });  
+  };
+
