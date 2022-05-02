5286df574e174ab63088d4ea3462e9b974200ab3
Raymundo12 <raymundo.olverap@gmail.com>
2022-05-02 08:38:29 +0000

    Mongoose

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const { default: mongoose } = require("mongoose");
      logger = require("morgan"),
      express = require ("express"), 
      bodyParser = require("body-parser");
@@ -12,14 +13,23 @@ app.use(require("./routes"));
 app.use(bodyParser.json());
 app.use(logger("tiny"));
 
-app.listen(port, function(err){
-    console.log("listening on port: "+ port)
-
 const dbURI = "mongodb://localhost/test";
 
 mongoose.connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true})
 .then((result) => console.log('connected to db'))
 .catch((err) => console.log(err));
+
+mongoose.connection.on('error', (err) => {
+    console.log('Mongodb Error:', err);
+    process.exit();
+});
+
+mongoose.connection.on('connected', () => {
+    console.log('MongoDB is successfully connected');
+});
+
+app.listen(port, function(err){
+    console.log("listening on port: "+ port)
 
 app.post('/users', userCtrl.createUser);
 app.get('/users', userCtrl.getUsers);
